0b7c0555935e439be2bbd30b89f5665d85bf399f
Bisclet <bisclet81234@gmail.com>
2023-10-03 16:28:50 +0200

    Set up

---
+++ Graphics/main.h
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <shader.h>
+#include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "model.h"
+#include "camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
---
+++ Simulation/MatrixTest.cpp
@@ -0,0 +1,15 @@
+#include "../include/FullMatrix.h"
+
+using namespace orbsim
+
+int main(){
+    FullMatrix* A = new FullMatrix(2,3);
+    FullMatrix* B = new FullMatrix(3,2);
+
+    for(int x = 0;x<3;x++){
+        for(int y = 0;y<2;y++){
+            insertValue()
+        }
+    }
+    return 0;
+}
